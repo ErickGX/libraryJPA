8ac43fde38878fe85307e3ef38a4166db8399e32
ErickGX <ericksaopaulino1@hotmail.com>
2025-08-05 04:58:01 -0300

    Metodo para filtrar pelos campos nome e nacionalidade de forma simplista

--- src/main/java/com/erickgx/libraryapi/controller/AutorController.java
+++ src/main/java/com/erickgx/libraryapi/controller/AutorController.java
@@ -4,15 +4,15 @@ package com.erickgx.libraryapi.controller;
 import com.erickgx.libraryapi.controller.dto.AutorDTO;
 import com.erickgx.libraryapi.models.Autor;
 import com.erickgx.libraryapi.service.AutorService;
-import org.apache.coyote.Response;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -55,6 +55,38 @@ public class AutorController {
 
          return ResponseEntity.notFound().build();
 
+
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletar(@PathVariable("id") String id){
+        var idAutor = UUID.fromString(id);
+        Optional<Autor> autorOptional = service.obterPorId(idAutor);
+
+        if(autorOptional.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        service.deletar(autorOptional.get());
+        return ResponseEntity.noContent().build();
+    }
+
+    //parametros com required false sao opcionais, todos sao true por padrao ao menos que mude para false
+    @GetMapping
+    public ResponseEntity<List<AutorDTO>> pesquisar(
+            @RequestParam(value = "nome", required = false) String nome,
+            @RequestParam(value = "nacionalidade", required = false) String nacionalidade){
+
+        List<Autor> resultados = service.pesquisa(nome, nacionalidade);
+        List<AutorDTO> LISTA = resultados.
+                stream()
+                .map(autor -> new AutorDTO(
+                    autor.getId(),
+                    autor.getNome(), autor.getDataNascimento(),
+                    autor.getNacionalidade())
+                ).collect(Collectors.toList());
+
+        return ResponseEntity.ok(LISTA);
 
     }
 }
--- src/main/java/com/erickgx/libraryapi/service/AutorService.java
+++ src/main/java/com/erickgx/libraryapi/service/AutorService.java
@@ -4,6 +4,7 @@ import com.erickgx.libraryapi.models.Autor;
 import com.erickgx.libraryapi.repository.AutorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -22,5 +23,25 @@ public class AutorService {
 
     public Optional<Autor> obterPorId(UUID id){
         return repository.findById(id);
+    }
+
+    public void deletar(Autor autor){
+        repository.delete(autor);
+    }
+
+    public List<Autor> pesquisa(String nome, String nacionalidade){
+        if(nome != null & nacionalidade != null){
+            return repository.findByNomeAndNacionalidade(nome, nacionalidade);
+        }
+
+        if (nome != null){
+            return repository.findByNome(nome);
+        }
+
+        if (nacionalidade != null){
+            return repository.findByNacionalidade(nacionalidade);
+        }
+
+        return repository.findAll();
     }
 }
