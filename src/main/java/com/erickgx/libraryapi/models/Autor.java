e45dae6c69e07911076d910ad95c79650921c8e9
ErickGX <ericksaopaulino1@hotmail.com>
2025-08-05 03:08:21 -0300

    Fix: resolvendo bug de conversao de UUID para Binary(16) no banco de dados MYSQL , para deixar o codigo flexivel para uso alternado do  PostGreSQL e MySQL WorkBench

--- src/main/java/com/erickgx/libraryapi/models/Autor.java
+++ src/main/java/com/erickgx/libraryapi/models/Autor.java
@@ -2,20 +2,28 @@ package com.erickgx.libraryapi.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Data
 @Entity
-@Table(name = "autor")
+@Table(name = "autor", schema = "public")
 @ToString(exclude = {"livros"})
+@EntityListeners(AuditingEntityListener.class)
+//config necessaria para criar um listener para os @CreatedDate e  @LastModifiedDate
+//ativar config @EnableJpaAuditing na classe de configuração ex : Application.Java ou outra
 public class Autor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "BINARY(16)") //HABILITAR QUANDO TIVER USANDO MYSQL E TIRAR QUANDO USAR POSTGRESS
     private UUID id;
 
     @Column(name = "nome", length = 100, nullable = false)
@@ -26,6 +34,18 @@ public class Autor {
 
     @Column(name = "nacionalidade", length = 50, nullable = false)
     private String nacionalidade;
+
+    @CreatedDate //Spring preenche a data automaticamente ao criar
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate //Spring preenche a data automaticamente ao atualizar
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @Column(name = "id_usuario")
+    private UUID idUsuario;
+
 
     //por padrao relacionamentos ToMany são Lazy fetch
     //@Transient
