c93436649cdb6a32853dad4e083c4acb697e519e
ErickGX <ericksaopaulino1@hotmail.com>
2025-10-19 15:57:35 -0300

    Adicionando lógica para cadastrar novos usuários via Google na classe LoginSocialSucessHandler, incluindo definição de senha padrão e extração de login a partir do email

--- src/main/java/com/erickgx/libraryapi/secutiry/LoginSocialSucessHandler.java
+++ src/main/java/com/erickgx/libraryapi/secutiry/LoginSocialSucessHandler.java
@@ -2,7 +2,6 @@ package com.erickgx.libraryapi.secutiry;
 
 import com.erickgx.libraryapi.models.Usuario;
 import com.erickgx.libraryapi.service.UsuarioService;
-import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,12 +14,14 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class LoginSocialSucessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
     private final UsuarioService usuarioService;
+    private static final String SENHA_PADRAO = "123456"; //senha padrao para usuarios via social login
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
@@ -37,6 +38,10 @@ public class LoginSocialSucessHandler extends SavedRequestAwareAuthenticationSuc
         //chamo a service para buscar por um usuario com este email fornecido pelo googleUser
         Usuario usuario = usuarioService.obterPorEmail(email);
 
+        if (usuario == null) {
+            usuario = CadastrarNovoUsuarioViaGoogle(email);
+        }
+
         //conversao para a nossa autenticacao customizada , conversao do userGoogle para o Usuario do sistema
         CustomAuthentication customAuthentication =  new CustomAuthentication(usuario);
 
@@ -45,6 +50,22 @@ public class LoginSocialSucessHandler extends SavedRequestAwareAuthenticationSuc
 
         //chamo o metodo da superclasse para continuar o fluxo normal com a autenticacao customizada
         super.onAuthenticationSuccess(request, response, customAuthentication);
+    }
+
+    private Usuario CadastrarNovoUsuarioViaGoogle(String email) {
+        Usuario usuario;
+        usuario =  new Usuario();
+        usuario.setEmail(email);
+        usuario.setLogin(obterLoginApartirEmail(email)); //capturando o login a partir do email
+        usuario.setSenha(SENHA_PADRAO); //definindo uma senha padrao para usuarios via social login
+        usuario.setRoles(List.of("OPERADOR"));
+
+        usuarioService.salvar(usuario);
+        return usuario;
+    }
+
+    private String obterLoginApartirEmail(String email) {
+        return email.substring(0, email.indexOf("@"));
     }
 
     //esta versao recebe o filterchain como parametro
